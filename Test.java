ed3ae471671affc5ef4f29b242d2e645f69a731d
Jael Evangelin <JaelEvangelin.R@flex.com>
2022-10-03 21:15:55 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,21 @@
+package mypackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=2;
+		int y=0;
+		for(;y<10;++y)
+		{
+			if(y%x==0)
+				continue;
+			else if(y==8)
+				break;
+			else
+				System.out.println(y+ "");
+		}
+		
+	}
+
+}
